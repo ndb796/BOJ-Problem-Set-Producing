80eaf77df902924ba63a49984e0db3e2727bb422
ndb796 <ndb796@naver.com>
2020-01-19 07:35:34 +0900

    Update

--- datasets/mock_exams/coding_test_A/problem_1/source_code.py
+++ datasets/mock_exams/coding_test_A/problem_1/source_code.py
@@ -1,11 +1,6 @@
-# -*- coding:utf-8 -*-
-file_name = 'data_15'
-input_file = open(file_name + '.in')
-input = input_file.readline
-
 n = int(input())
 a = list(map(int, input().split()))
 a.sort()
  
-# Median(°¡¿îµ¥) °ªÀ» Ãâ·ÂÇÕ´Ï´Ù.
+# Median(ê°€ìš´ë°) ê°’ì„ ì¶œë ¥í•©ë‹ˆë‹¤.
 print(a[(n - 1) // 2])
